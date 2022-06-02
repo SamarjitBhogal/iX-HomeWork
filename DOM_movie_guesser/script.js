e534789f89283260bc8ad9578692fa1918eeaa33
Samarjit Bhogal <samar288@students.cs.ubc.ca>
2022-06-02 16:56:28 -0700

    finished movie guesser home work.

--- DOM_movie_guesser/script.js
+++ DOM_movie_guesser/script.js
@@ -11,4 +11,41 @@ const movies = [
     {title: 'Peter Pan', explanation: 'In this movie some kids die and an angel escorts them to heaven...', hint: 'Always flying, cuz he neverlands'},
     {title: 'The Lord Of The Rings', explanation: 'In this movie some small guys go for a walk...', hint: 'You will not vacate past this exact position'}
    ]
-   
+
+var i = 0;
+
+const input = document.getElementById('input');
+const submit = document.getElementById('submit'); 
+const hint = document.getElementById('hint');
+const movieText = document.getElementById('movie-text');
+const card = document.getElementById('card');
+const cardElement = document.createElement('div');
+
+movieText.innerHTML = movies[i].explanation;
+cardElement.classList.add('alert');
+
+submit.addEventListener('click', (event) => {
+
+    if (input.value == movies[i].title) {
+        cardElement.classList.add('alert-success');
+        cardElement.innerHTML = "You guessed correctly the movie was " +  movies[i].title;
+
+        input.value = '';
+        i++;
+        movieText.innerHTML = movies[i].explanation;
+    } else {
+        cardElement.classList.add('alert-danger'); 
+        cardElement.innerHTML = "You guessed incorrectly the movie was " +  movies[i].title;
+
+        input.value = '';
+        i++;
+        movieText.innerHTML = movies[i].explanation;
+    }
+    card.appendChild(cardElement);
+});
+
+hint.addEventListener('click', (event) => {
+    cardElement.classList.add('alert-info');
+    cardElement.innerHTML = movies[i].hint;
+    card.appendChild(cardElement);
+});
